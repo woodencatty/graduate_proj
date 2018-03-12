3beb754e625378fb574945068cb12c1ff313dc24
Woodencatty <woodencatty@gmail.com>
2018-03-12 15:32:14 +0900

    fix code

--- WearableCode/node_accel_graph_file.js
+++ WearableCode/node_accel_graph_file.js
@@ -5,19 +5,12 @@ const fs = require('fs');
 
 
 setInterval(()=>{
-let log = "" + accel.AccelX()+","+accel.AccelY()+","+accel.AccelZ()+","+accel.GyroX()+","+accel.GyroY()+","+accel.GyroZ()+","+accel.MagnX()+","+accel.MagnY()+","+accel.MagnZ()+"\n"
+let log = accel.AccelX()+","+accel.AccelY()+","+accel.AccelZ()+","+accel.GyroX()+","+accel.GyroY()+","+accel.GyroZ()+","+accel.MagnX()+","+accel.MagnY()+","+accel.MagnZ()+"\n"
 console.log(log);
 
-fs.open('data_log.txt', 'a+', function(err, fd) {
-    if(err) throw err;
-    fs.write(fd, log, 0, log.length, null, function(err, written, buffer) {
-      if(err) throw err;
-      console.log(err, written, buffer);
-      fs.close(fd, function() {
-        console.log('Done');
-      });
-    });
-  });
+fs.appendFile('data_log.txt', log, (err) => {
+  if (err) throw err;
+});
 
 }, 50);
 
