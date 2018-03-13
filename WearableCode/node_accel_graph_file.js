896232de4ae3463fb99a1e8c409933aa2b0d71b8
Woodencatty <woodencatty@gmail.com>
2018-03-13 15:12:36 +0900

    after 10second

--- WearableCode/node_accel_graph_file.js
+++ WearableCode/node_accel_graph_file.js
@@ -3,15 +3,17 @@
 const accel = require('./build/Release/accel');
 const fs = require('fs');
 
+setTimeout(()=>{ 
+  setInterval(()=>{
+  let log = accel.AccelX()+","+accel.AccelY()+","+accel.AccelZ()+","+accel.GyroX()+","+accel.GyroY()+","+accel.GyroZ()+","+accel.MagnX()+","+accel.MagnY()+","+accel.MagnZ()+"\n"
+  console.log(log);
+  
+  fs.appendFile('data_log.txt', log, (err) => {
+    if (err) throw err;
+  });
+  
+  }, 50);
+ }, 10000);
 
-setInterval(()=>{
-let log = accel.AccelX()+","+accel.AccelY()+","+accel.AccelZ()+","+accel.GyroX()+","+accel.GyroY()+","+accel.GyroZ()+","+accel.MagnX()+","+accel.MagnY()+","+accel.MagnZ()+"\n"
-console.log(log);
-
-fs.appendFile('data_log.txt', log, (err) => {
-  if (err) throw err;
-});
-
-}, 50);
 
 setTimeout(()=>{ process.exit(1); }, 300000);
