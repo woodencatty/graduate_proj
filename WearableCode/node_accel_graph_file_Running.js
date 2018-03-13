c7cd9c5e0b32211347da4a4dce805bdde9d56c96
Woodencatty <woodencatty@gmail.com>
2018-03-13 16:10:44 +0900

    devide checks

---
+++ WearableCode/node_accel_graph_file_Running.js
@@ -0,0 +1,19 @@
+
+// accel_graph.js
+const accel = require('./build/Release/accel');
+const fs = require('fs');
+
+setTimeout(()=>{ 
+  setInterval(()=>{
+  let log = accel.AccelX()+"\t"+accel.AccelY()+"\t"+accel.AccelZ()+"\t"+accel.GyroX()+"\t"+accel.GyroY()+"\t"+accel.GyroZ()+"\t"+accel.MagnX()+"\t"+accel.MagnY()+"\t"+accel.MagnZ()+"Running\n"
+  console.log(log);
+  
+  fs.appendFile('data_log_Running.txt', log, (err) => {
+    if (err) throw err;
+  });
+  
+  }, 100);
+ }, 20000);
+
+
+setTimeout(()=>{ process.exit(1); }, 50000);
