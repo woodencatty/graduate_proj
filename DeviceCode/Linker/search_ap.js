6a56f3f69506173e62a6b6d14e2c67468085067c
Woodencatty <woodencatty@gmail.com>
2018-05-28 13:02:05 +0900

    test

--- DeviceCode/Linker/search_ap.js
+++ DeviceCode/Linker/search_ap.js
@@ -14,9 +14,9 @@ wifi.init({
 module.exports = {
     searchPoster: (apName, password, connectRange, leaveRange) => {
         wifi.getCurrentConnections((err, curcon) => {
-            console.log(curcon[0].signal_level);
+            console.log(curcon);
             //todo : check signal
-            if (curcon[0].signal_level > connectRange && searched == false) {
+         /*   if (curcon[0].signal_level > connectRange && searched == false) {
                 sendData.SubmitIDDname('Linker01');
                 fs.readFile('./exercise_log', 'utf8', function (error, readtext) { sendData.SubmitUserExercise('Linker01', readtext.toString());
 exec("sudo rm exercise_log", function (error, stdout, stderr) {
@@ -28,8 +28,8 @@ exec("sudo rm exercise_log", function (error, stdout, stderr) {
             if (curcon[0].signal_level < leaveRange && searched == true) {
                 sendData.SubmitUserLeave();
                 searched = false;
-            }
-        });
+            }*/
+        });   
     }
 }
 
