044bcad5aeaf00763cb8ca1dde94c95ebd81229b
Woodencatty <woodencatty@gmail.com>
2018-06-18 09:42:19 +0900

    sql update

--- DeviceCode/SmartPoster/routes/index.js
+++ DeviceCode/SmartPoster/routes/index.js
@@ -23,27 +23,26 @@ function Setup_IDD_Socket() {
       if (request.url == '/identify/information') {
         IDD_ID = request.headers.idd_id;
         response.writeHead(200);
-        response.end("gotit");    //IDD에 확인메세지 전송
-        console.log("Hi! " + IDD_ID);   //환자 식별
+        response.end("gotit"); //IDD에 확인메세지 전송
+        console.log("Hi! " + IDD_ID); //환자 식별
       } else if (request.url == '/patient/exercise') {
-        response.writeHead(200);  
-     //   restAPI.SubmitUserExercise(request.headers.idd_id, request.headers.exercise);
+        response.writeHead(200);
+        //   restAPI.SubmitUserExercise(request.headers.idd_id, request.headers.exercise);
         console.log(request.headers.exercise);
         console.log(request.headers.idd_id);
-        IDD_ID = request.headers.idd_id;        
+        IDD_ID = request.headers.idd_id;
         response.end("gotit");
-    //    restAPI.SubmitUserExercise(request.headers.idd_id, request.headers.exercise);
+        //    restAPI.SubmitUserExercise(request.headers.idd_id, request.headers.exercise);
       } else if (request.url == '/patient/leave') {
         response.writeHead(200);
         response.end("good-bye");
         let IDD_ID = "";
-      }
-      else {
+      } else {
         console.log("error");
         response.writeHead(404);
         response.end();
       }
-    } 
+    }
   }).listen(3010, () => {
     console.log('Socket is Running (3010) ...');
   });
@@ -55,7 +54,7 @@ function initialize() {
     var config = JSON.parse(data);
     Setup_IDD_Socket();
     // AP.setupAP(config.ssid, config.password, true, config.adaptor);
-      interval = config.refreshInterval;
+    interval = config.refreshInterval;
     APD_ID = config.deviceName;
   });
   console.log("Page is Running..(3000)");
@@ -67,25 +66,32 @@ router.get('/', function (req, res, next) {
   console.log("routed to /")
   Statuscallback = (returnData) => {
 
-  /*  detectcallback = (deviceName)=>{
-      IDD_ID = deviceName;
-    }
-    bluetooth.IDD_found(detectcallback);*/
-    
+    /*  detectcallback = (deviceName)=>{
+        IDD_ID = deviceName;
+      }
+      bluetooth.IDD_found(detectcallback);*/
+
     console.log("get data : " + returnData);
-    if(returnData == "1"){
+    if (returnData == "1") {
       if (IDD_ID == "") {
-        res.render('index', { Interval: refreshInterval, temp: 0, humi: 0});
-        
-       /* sensorcallback = (temp, humi)=>{
-          res.render('index', { Interval: refreshInterval, temp: temp, humi: humi });
-        }
-        sensor.getTemp(sensorcallback);*/
+        res.render('index', {
+          Interval: refreshInterval,
+          temp: 0,
+          humi: 0
+        });
+
+        /* sensorcallback = (temp, humi)=>{
+           res.render('index', { Interval: refreshInterval, temp: temp, humi: humi });
+         }
+         sensor.getTemp(sensorcallback);*/
       } else {
         res.redirect('/detected');
-      }}else {res.redirect('/unactivated');}
+      }
+    } else {
+      res.redirect('/unactivated');
+    }
   }
-  sql.requestDeviceStatus("poster01",  Statuscallback);
+  sql.requestDeviceStatus("poster01", Statuscallback);
 
 });
 
@@ -96,46 +102,51 @@ router.get('/unactivated', function (req, res, next) {
 router.get('/detected', function (req, res, next) {
   Identifycallback = (returnData) => {
     User_Name = returnData; // 환자이름 빼먹음;
-    var tts_query = "안녕하세요! "+User_Name+"님! 같이 운동 해볼까요?"
-    res.render('detected', { username: User_Name, query: tts_query});
+    var tts_query = "안녕하세요! " + User_Name + "님! 같이 운동 해볼까요?"
+    res.render('detected', {
+      username: User_Name,
+      query: tts_query
+    });
   }
-  sql.requestUserName(IDD_ID,Identifycallback);
+  sql.requestUserName(IDD_ID, Identifycallback);
 });
 
 router.get('/search_exercise', function (req, res, next) {
   dentifycallback = (returnData) => {
     User_Exercise = returnData; // 환자이름 빼먹음;
-    if(returnData == "exercise_walk"){
-      res.redirect('/exercise_walk');  
-    }else if(returnData == "end"){
-    
-    }else{
+    if (returnData == "exercise_walk") {
+      res.redirect('/exercise_walk');
+    } else if (returnData == "end") {
+
+    } else {
       res.redirect('/exercise');
     }
-    
+
   }
-  sql.requestUserExercise(IDD_ID,dentifycallback);
+  sql.requestUserExercise(IDD_ID, dentifycallback);
 });
 
 router.get('/reset', function (req, res, next) {
   IDD_ID = "";
   User_Exercise = "";
   User_Name = "";
-  
+
   res.render('reset');
 });
 
 router.get('/exercise', function (req, res, next) {
 
-    res.render('exercise', {image: "http://192.9.44.54:8081/smash/resources/img/programimg/programImg_"+User_Exercise+".png"});
-  
+  res.render('exercise', {
+    image: "http://192.9.44.54:8081/smash/resources/img/programimg/programImg_" + User_Exercise + ".png"
+  });
+
 });
 
 
 router.get('/exercise_walk', function (req, res, next) {
 
   res.render('exercise_walk');
-  
+
 });
 
 
