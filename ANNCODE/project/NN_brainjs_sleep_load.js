1b48656c9a94caebde05233cd3efb6d4f7acd990
woodencatty <woodencatty@gmail.com>
2018-06-22 01:39:50 +0900

    ?

--- ANNCODE/project/NN_brainjs_sleep_load.js
+++ ANNCODE/project/NN_brainjs_sleep_load.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var net = new brain.NeuralNetwork();
 
 function loadFile() {
-    var obj = JSON.parse(fs.readFileSync('network.json', 'utf8'));
+    var obj = JSON.parse(fs.readFileSync('sleep.json', 'utf8'));
     net.fromJSON(obj);
     console.log("file loaded");
 }
@@ -12,9 +12,7 @@ function loadFile() {
 loadFile(); 
 
 
-var output = net.run({x축_가속도: -335, y축_가속도: -433, z축_가속도: 112, 
-    x축_자이로: -335, y축_자이로: -433, z축_자이로: 112, 
-    x축_지자기: -977, y축_지자기: 964, z축_지자기: 1170});   // Walking Data
+var output = net.run({습도: 72, 소음: 45, 온도: 14, 밝기: 2});   // Walking Data
 
 console.log(output);
 
