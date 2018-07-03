355eaf572dc5662c71ac05e065e2443fedbec290
Woodencatty <woodencatty@gmail.com>
2018-07-03 14:59:15 +0900

    yers!

--- TestZone/wifitst.js
+++ TestZone/wifitst.js
@@ -7,26 +7,26 @@ wifi.init({
 });
  
 // Scan networks
-wifi.scan(function(err, networks) {
+wifi.getCurrentConnections(function(err, currentConnections) {
     if (err) {
         console.log(err);
-    } else {
-        console.log(networks);
-        /*
-        networks = [
-            {
-              ssid: '...',
-              bssid: '...',
-              mac: '...', // equals to bssid (for retrocompatibility)
-              channel: <number>,
-              frequency: <number>, // in MHz
-              signal_level: <number>, // in dB
-              security: 'WPA WPA2' // format depending on locale for open networks in Windows
-              security_flags: '...' // encryption protocols (format currently depending of the OS)
-              mode: '...' // network mode like Infra (format currently depending of the OS)
-            },
-            ...
-        ];
-        */
     }
+    console.log(currentConnections);
+    /*
+    // you may have several connections
+    [
+        {
+            iface: '...', // network interface used for the connection, not available on macOS
+            ssid: '...',
+            bssid: '...',
+            mac: '...', // equals to bssid (for retrocompatibility)
+            channel: <number>,
+            frequency: <number>, // in MHz
+            signal_level: <number>, // in dB
+            security: '...' //
+            security_flags: '...' // encryption protocols (format currently depending of the OS)
+            mode: '...' // network mode like Infra (format currently depending of the OS)
+        }
+    ]
+    */
 });
