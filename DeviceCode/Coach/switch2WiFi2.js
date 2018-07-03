48c1838e01120022bc37492b5b8e66a43ae44d16
Woodencatty <woodencatty@gmail.com>
2018-07-03 18:45:51 +0900

    make iyt

---
+++ DeviceCode/Coach/switch2WiFi2.js
@@ -0,0 +1,15 @@
+var wifi = require('node-wifi');
+ 
+// Initialize wifi module
+// Absolutely necessary even to set interface to null
+wifi.init({
+    iface : null // network interface, choose a random wifi interface if set to null
+});
+ 
+            wifi.connect({ ssid : "WiFi2", password : "201333558"}, function(err) {
+                if (err) {
+                    console.log(err);
+                }
+                console.log('Connected');
+            });
+    
