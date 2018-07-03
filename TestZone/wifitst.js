335fa49d8f6b27d7a6006c92122b699070d37d2e
Woodencatty <woodencatty@gmail.com>
2018-07-03 14:59:40 +0900

    setup

--- TestZone/wifitst.js
+++ TestZone/wifitst.js
@@ -6,27 +6,29 @@ wifi.init({
     iface : null // network interface, choose a random wifi interface if set to null
 });
  
-// Scan networks
-wifi.getCurrentConnections(function(err, currentConnections) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(currentConnections);
-    /*
-    // you may have several connections
-    [
-        {
-            iface: '...', // network interface used for the connection, not available on macOS
-            ssid: '...',
-            bssid: '...',
-            mac: '...', // equals to bssid (for retrocompatibility)
-            channel: <number>,
-            frequency: <number>, // in MHz
-            signal_level: <number>, // in dB
-            security: '...' //
-            security_flags: '...' // encryption protocols (format currently depending of the OS)
-            mode: '...' // network mode like Infra (format currently depending of the OS)
+setInterval(()=>{
+    wifi.getCurrentConnections(function(err, currentConnections) {
+        if (err) {
+            console.log(err);
         }
-    ]
-    */
-});
+        console.log(currentConnections);
+        /*
+        // you may have several connections
+        [
+            {
+                iface: '...', // network interface used for the connection, not available on macOS
+                ssid: '...',
+                bssid: '...',
+                mac: '...', // equals to bssid (for retrocompatibility)
+                channel: <number>,
+                frequency: <number>, // in MHz
+                signal_level: <number>, // in dB
+                security: '...' //
+                security_flags: '...' // encryption protocols (format currently depending of the OS)
+                mode: '...' // network mode like Infra (format currently depending of the OS)
+            }
+        ]
+        */
+    });
+}, 1000)
+// Scan networks
